845bf597a062c6681c9d2c4a205399c8a49161b1
Thokozani Masanabo <thokozani26sngalela@gmail.com>
2025-01-21 13:09:40 +0200

    Populated the fault type table

--- FixIt/reports/models.py
+++ FixIt/reports/models.py
@@ -6,6 +6,7 @@ from django.conf import settings
 class FaultType(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    importances = models.PositiveIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
