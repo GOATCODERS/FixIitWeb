a49e9995ffb8cc1eeae801f1a48a36e6925644f4
Thokozani Masanabo <86426035+GOATCODERS@users.noreply.github.com>
2025-03-14 00:12:44 +0200

    Delete FixIt directory

--- FixIt/appointments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('schedule/<int:report_id>/', views.schedule_appointment, name='schedule_appointment'),
-    path('list/', views.appointment_list, name='appointment_list'),
-]
--- FixIt/reports/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('report/', views.report_fault, name='report_fault'),
-    path('report/list/', views.list_reports, name='list_reports'),
-    path('reports/<int:report_id>/', views.report_detail, name='report_detail'),
-    # path('admin/reports/', views.admin_reports, name='admin_reports'),
-]
